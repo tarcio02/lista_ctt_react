dc39e1a3be4651258c1db2a8474c53fc319e9dbe
tarcio teixeia <tarciogbteixeira@gmail.com>
2024-03-04 10:16:50 -0300

    finalizando setup

---
+++ src/styles/index.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const EstiloGlobal = createGlobalStyle'
+*{
+  margin: 0;
+  padding:0;
+  box-sizing: border-Box;
+  font-family: sans-serif;
+}'
